a8cdb86e073837302220661dd95ba587ed5fc9ce
Abhijeet Sen <abhijeetsen661@gmail.com>
2024-12-23 13:09:56 +0530

    updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
-import NotesCard from '../../components/noteCard/NoteCard'
+import NotesCard from '../../components/noteCard/NotesCard'
 
 function Home() {
   return (
