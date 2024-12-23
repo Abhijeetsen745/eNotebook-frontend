1bbdf43b7c291ce66e0188412579d49b38037837
Abhijeet Sen <abhijeetsen661@gmail.com>
2024-12-23 12:30:23 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import NotesCard from '../../components/noteCard/NoteCard'
+
+function Home() {
+  return (
+   <Layout>
+     <NotesCard/>
+   </Layout>
+  )
+}
+
+export default Home
